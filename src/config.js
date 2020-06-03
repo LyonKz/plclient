dbe985ba958774a91028e77fbafb328271d29644
LyonKz <38329746+LyonKz@users.noreply.github.com>
2020-06-03 16:45:23 +0800

    Setting up our React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "vnebucket"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://e55y5yz9ld.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_ebbeFiDV4",
+      APP_CLIENT_ID: "16akh3t1c0uitqb2mh3o62qv1o",
+      IDENTITY_POOL_ID: "us-east-2:6191b0fd-d33c-43b5-b159-5f796409e96c"
+    }
+  };
