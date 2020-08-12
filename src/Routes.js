c544d592af0f1797b342ce750a65d2c75abaf6db
LyonKz <38329746+LyonKz@users.noreply.github.com>
2020-08-12 11:40:46 +0800

    DDD

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,13 @@ import {
 } from "react-router-dom";
 import Home from "./container/Home";
 import NotFound from "./container/NotFound";
+import Login from "./container/Login";
+import Signup from "./container/Signup";
+import NewNote from "./container/NewNote";
+import Notes from "./container/Notes.js";
+import Settings from "./container/Settings";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 export default function Routes() {
     return ( 
@@ -12,6 +19,21 @@ export default function Routes() {
             <Route exact path = "/">
                 <Home/> 
             </Route>
+            <UnauthenticatedRoute exact path="/login">
+                <Login />
+            </UnauthenticatedRoute>
+            <UnauthenticatedRoute exact path="/signup">
+                <Signup />
+            </UnauthenticatedRoute>
+            <AuthenticatedRoute exact path="/settings">
+                <Settings />
+            </AuthenticatedRoute>
+            <AuthenticatedRoute exact path="/notes/new">
+                <NewNote />
+            </AuthenticatedRoute>
+            <AuthenticatedRoute exact path="/notes/:id">
+                <Notes />
+            </AuthenticatedRoute>
             <Route>
                 <NotFound />
             </Route>
--- src/config.js
+++ src/config.js
@@ -1,16 +1,17 @@
 export default {
+    MAX_ATTACHMENT_SZE: 5000000,
     s3: {
       REGION: "us-east-2",
-      BUCKET: "vnebucket"
+      BUCKET: "profesbucket"
     },
     apiGateway: {
       REGION: "us-east-2",
-      URL: "https://e55y5yz9ld.execute-api.us-east-2.amazonaws.com/prod"
+      URL: "https://jj9ulnm812.execute-api.us-east-2.amazonaws.com/prod"
     },
     cognito: {
       REGION: "us-east-2",
-      USER_POOL_ID: "us-east-2_ebbeFiDV4",
-      APP_CLIENT_ID: "16akh3t1c0uitqb2mh3o62qv1o",
-      IDENTITY_POOL_ID: "us-east-2:6191b0fd-d33c-43b5-b159-5f796409e96c"
+      USER_POOL_ID: "us-east-2_yEl31Dije",
+      APP_CLIENT_ID: "3djlusueq62n52j43ktc10da6i",
+      IDENTITY_POOL_ID: "us-east-2:b0168ad3-2217-4cd8-bd42-04a701a7d850"
     }
   };
